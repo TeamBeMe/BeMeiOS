8d7def672054fdde3b8159eab18c677754362b7e
qodhrkawk <gabriel1120@naver.com>
2020-12-28 20:06:02 +0900

    :bulb: Home storyboard,VC,Cell 파일 생성

---
+++ BeMe/BeMe/Home/VCs/HomeVC.swift
@@ -0,0 +1,29 @@
+//
+//  HomeVC.swift
+//  BeMe
+//
+//  Created by Yunjae Kim on 2020/12/28.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var cardCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    
+    
+    
+    
+
+
+}
