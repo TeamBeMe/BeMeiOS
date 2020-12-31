3cb62d378189f342fe4b14332ce6586e8f280567
jaeyong <maxwodyd98@gmail.com>
2020-12-31 20:18:52 +0900

    :hammer: 화면 구조 잡기

--- BeMe/BeMe/Explore/VCs/ExploreVC.swift
+++ BeMe/BeMe/Explore/VCs/ExploreVC.swift
@@ -9,21 +9,61 @@ import UIKit
 
 class ExploreVC: UIViewController {
 
+    //MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        adjustScrollViewInset()
+    }
+
+    
+    //MARK: - IBOulets
+    @IBOutlet weak var exploreScrollView: UIScrollView!
+    @IBOutlet weak var highLightBar: UIView!
+    @IBOutlet weak var highLightBarLeading: NSLayoutConstraint!
+    
+    //MARK: - IBActions
+
+    @IBAction func recentButtonTapped(_ sender: UIButton) {
+        moveHighLightBar(to: sender)
+    }
+    @IBAction func favoriteButtonTapped(_ sender: UIButton) {
+        moveHighLightBar(to: sender)
+    }
+}
+
+extension ExploreVC {
+    //MARK: - private Method
+    
+    private func moveHighLightBar(to button: UIButton) {
+        
+        UIView.animate(withDuration: 0.2, delay: 0, options: [.curveLinear], animations: {
+            
+            // Slide Animation
+            self.highLightBar.frame.origin.x = 30 + button.frame.minX
+            
+            // FadeIn Animation
+            //self.highLightBarLeading.constant = 30 + button.frame.minX
+        }) { _ in
+            
+        }
+    }
+
+    private func adjustScrollViewInset() {
+        if #available(iOS 11.0, *) {
+            exploreScrollView.contentInsetAdjustmentBehavior = .never
+        } else {
+            automaticallyAdjustsScrollViewInsets = false
+        }
+        
+    }
+}
+
+extension ExploreVC: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        print("~~~> offset\(exploreScrollView.contentOffset)")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
