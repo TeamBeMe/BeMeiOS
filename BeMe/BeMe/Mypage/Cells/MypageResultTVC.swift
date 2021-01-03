c7cc7762050b89bf4046ed53407aab1a28a4995e
sery270 <sery270@naver.com>
2021-01-03 16:44:49 +0900

    💡Cell 파일 생성

---
+++ BeMe/BeMe/Mypage/Cells/MypageResultTVC.swift
@@ -0,0 +1,23 @@
+//
+//  MypageResultTVC.swift
+//  BeMe
+//
+//  Created by 박세란 on 2021/01/03.
+//
+
+import UIKit
+
+class MypageResultTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ BeMe/BeMe/Mypage/Cells/MypageSearchTVC.swift
@@ -0,0 +1,23 @@
+//
+//  MypageSearchTVC.swift
+//  BeMe
+//
+//  Created by 박세란 on 2021/01/03.
+//
+
+import UIKit
+
+class MypageSearchTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
