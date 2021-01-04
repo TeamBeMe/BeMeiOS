6b4ffcbee125f98653194189e330547460b1bb8e
sery270 <sery270@naver.com>
2021-01-04 18:16:14 +0900

    🔨MypageSearchTVC 작성

--- BeMe/BeMe/Mypage/Cells/MypageResultTVC.swift
+++ BeMe/BeMe/Mypage/Cells/MypageResultTVC.swift
@@ -8,6 +8,24 @@
 import UIKit
 
 class MypageResultTVC: UITableViewCell {
+    
+    //MARK:**- IBOutlet Part**
+
+    /// Label, ColelctionView, TextField, ImageView 등 @IBOutlet 변수들을 선언합니다.  // 변수명 lowerCamelCase 사용
+
+    /// ex)  @IBOutlet weak var qnaTextBoxBackgroundImage: UIImageView!
+
+    //MARK:**- Variable Part**
+
+    /// 뷰컨에 필요한 변수들을 선언합니다  // 변수명 lowerCamelCase 사용
+
+    /// ex)  var imageViewList : [UIImageView] = []
+    
+    //MARK:**- Life Cycle Part**
+
+    /// 앱의 Life Cycle 부분을 선언합니다
+
+    /// ex) override func viewWillAppear() { }
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -19,5 +37,35 @@ class MypageResultTVC: UITableViewCell {
 
         // Configure the view for the selected state
     }
+
+    //MARK:**- IBAction Part**
+
+    /// 버튼과 같은 동작을 선언하는 @IBAction 을 선언합니다 , IBAction 함수 명은 동사 형태로!!  // 함수명 lowerCamelCase 사용
+
+    /// ex) @IBOutlet @IBAction func answerSelectedButtonClicked(_ sender: Any) { }
+
+    //MARK:**- default Setting Function Part**
+
+    /// 기본적인 세팅을 위한 함수 부분입니다 // 함수명 lowerCamelCase 사용
+
+    /// ex) func tableViewSetting() {
+
+    ///         myTableView.delegate = self
+
+    ///         myTableView.datasource = self
+
+    ///    }
+
+    //MARK:**- Function Part**
+
+    /// 로직을 구현 하는 함수 부분입니다. // 함수명 lowerCamelCase 사용
+
+    /// ex) func tableViewSetting() {
+
+    ///         myTableView.delegate = self
+
+    ///         myTableView.datasource = self
+
+    ///    }
 
 }
--- BeMe/BeMe/Mypage/Cells/MypageSearchTVC.swift
+++ BeMe/BeMe/Mypage/Cells/MypageSearchTVC.swift
@@ -8,16 +8,70 @@
 import UIKit
 
 class MypageSearchTVC: UITableViewCell {
-
+    
+    //MARK:**- IBOutlet Part**
+    @IBOutlet weak var searchView: UIView!
+    
+    @IBOutlet weak var keywordLabel: UILabel!
+    
+    //MARK:**- Variable Part**
+    
+    lazy var identifier: String = "MypageSearchTVC"
+    
+    
+    
+    
+    //MARK:**- Life Cycle Part**
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        setSearhView(view: searchView)
+        setKeywordLabel(label: keywordLabel)
+        
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
+    //MARK:**- IBAction Part**
+    
+    @IBAction func deleteButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func filterButtonTapped(_ sender: Any) {
+    }
+    
+    
+    //MARK:**- default Setting Function Part**
+    
+    
+    func setSearhView(view: UIView) {
+        view.setBorderWithRadius(borderColor: .veryLightPinkTwo, borderWidth: 1, cornerRadius: 6)
+        view.backgroundColor = UIColor.veryLightPinkTwo
+    }
+    
+    
+    // 아래 두 함수는 TVC 뿐만 아니라 여러 곳에서 사용가능
+    // 검색어를 삭제했거나 , 초기 화면
+    func setKeywordLabel(label : UILabel){
+        label.text = "검색"
+        label.textColor = UIColor.rgb8E8E93
+    }
+    
+    // 검색 결과 후
+    func setKeywordLabel( label : UILabel, keyword: String){
+        label.text = keyword
+        label.textColor = UIColor.darkGray
+        
+    }
+    
+    
+    
+    //MARK:**- Function Part**
+    
+    
 }
