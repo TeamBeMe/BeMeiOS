bf8d27b25ba29ad62087620dacf2bfa52868da17
sery270 <sery270@naver.com>
2021-01-04 20:52:13 +0900

    💡Cell 파일 생성

---
+++ BeMe/BeMe/Mypage/Cells/MypageOthersScrapTVC.swift
@@ -0,0 +1,37 @@
+//
+//  MypageOthersScrapTVC.swift
+//  BeMe
+//
+//  Created by 박세란 on 2021/01/04.
+//
+
+import UIKit
+
+class MypageOthersScrapTVC: UITableViewCell {
+    
+    //MARK:**- IBOutlet Part**
+    
+    
+    //MARK:**- Variable Part**
+    
+    
+    //MARK:**- Life Cycle Part**
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    //MARK:**- IBAction Part**
+    
+    //MARK:**- default Setting Function Part**
+    
+    //MARK:**- Function Part**
+    
+}
