576f7bb4f1f521652284cccd8986c840ac55dbc1
jaeyong <maxwodyd98@gmail.com>
2021-01-05 17:48:38 +0900

    :bulb: comment 폴더 생성

---
+++ BeMe/BeMe/Comment/VCs/CommentVC.swift
@@ -0,0 +1,29 @@
+//
+//  CommentVC.swift
+//  BeMe
+//
+//  Created by 이재용 on 2021/01/05.
+//
+
+import UIKit
+
+class CommentVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
