9e13d7f7a68e02b3428895900bd8ae4c86f3da59
jaeyong <maxwodyd98@gmail.com>
2021-01-05 21:02:53 +0900

    :bulb: Cell 파일 추가

---
+++ BeMe/BeMe/Comment/Cells/CommentTVC.swift
@@ -0,0 +1,24 @@
+//
+//  CommentTVC.swift
+//  BeMe
+//
+//  Created by 이재용 on 2021/01/05.
+//
+
+import UIKit
+
+class CommentTVC: UITableViewCell {
+    static let identifier: String = "CommentCVC"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- BeMe/BeMe/Comment/VCs/CommentVC.swift
+++ BeMe/BeMe/Comment/VCs/CommentVC.swift
@@ -9,21 +9,56 @@ import UIKit
 
 class CommentVC: UIViewController {
 
+    @IBOutlet weak var informationView: UIView!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var cateogryLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nickNameLabel: UILabel!
+    @IBOutlet weak var answerTextView: UITextView!
+    @IBOutlet weak var moreAnswerButton: UIButton!
+    @IBOutlet weak var commentTableView: UITableView!
+    
+    private var commentArray = [{}]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setView()
+        setTableView()
+        moreAnswerButton.isHidden = true
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+//MARK: - Private Method
+extension CommentVC {
+    
+    private func setView() {
+        
     }
-    */
+    
+    private func setTableView() {
+        commentTableView.delegate = self
+        commentTableView.dataSource = self
+    }
+}
 
+//MARK: - UITableViewDelegate
+
+extension CommentVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return commentArray.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return comment
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    
 }
