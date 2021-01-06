619fcb863965958dd36aa6bd97148621c85d6890
jaeyong <maxwodyd98@gmail.com>
2021-01-06 18:34:50 +0900

    :hammer: comment tableview 오토레이아웃 잡기

--- BeMe/BeMe/Comment/VCs/CommentTestVC.swift
+++ BeMe/BeMe/Comment/VCs/CommentTestVC.swift
@@ -12,7 +12,7 @@ class CommentTestVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
     }
     
 
