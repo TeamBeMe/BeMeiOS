f5754738d36abb3974ab2423ee5e236b1c0765da
jaeyong <maxwodyd98@gmail.com>
2021-01-06 21:07:09 +0900

    :hammer: CommentVC 구성

--- BeMe/BeMe/Comment/VCs/CommentVC.swift
+++ BeMe/BeMe/Comment/VCs/CommentVC.swift
@@ -1,32 +1,30 @@
 //
-//  CommentVC.swift
+//  CommentTestVC.swift
 //  BeMe
 //
-//  Created by 이재용 on 2021/01/05.
+//  Created by 이재용 on 2021/01/06.
 //
 
 import UIKit
 
+//MARK: - Comment 모델
 
-
+struct CommentA {
+    let comment: String
+    let children: [CommentA]?
+    var open: Bool
+}
 class CommentVC: UIViewController {
     
-    @IBOutlet weak var informationView: UIView!
-    @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var cateogryLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var profileImageView: UIImageView!
-    @IBOutlet weak var nickNameLabel: UILabel!
-    @IBOutlet weak var answerTextView: UITextView!
-    @IBOutlet weak var moreAnswerButton: UIButton!
     @IBOutlet weak var commentTableView: UITableView!
-    @IBOutlet weak var commentTableViewHeight: NSLayoutConstraint!
-    @IBOutlet weak var dateBottomConstraint: NSLayoutConstraint!
-    @IBOutlet weak var profileBottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var scrapButton: UIButton!
     
     lazy var popupBackgroundView: UIView = UIView()
     
+    var isMoreButtonHidden: Bool = true
+    
+    var isMyAnswer: Bool = true
+        
     var isScrapped: Bool = false {
         didSet {
             
@@ -45,28 +43,14 @@ class CommentVC: UIViewController {
                                                 CommentA(comment: "오! 안녕!", children: [], open: false)], open: false),
     ]
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setView()
-        setTableView()
+        setPopupView()
+        setCommentTableView()
         setNotificationCenter()
-        
-        // 디테일페이지에서 들어왔을 경우
-        moreAnswerButton.isHidden = true
-        
-        // 내 글일 경우
-        dateBottomConstraint.constant = 14
-        dateBottomConstraint.priority = UILayoutPriority(1000)
-        profileBottomConstraint.priority = UILayoutPriority(999)
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        adjustTableViewHeight()
-    }
+    
     
     @IBAction func dismissButtonTapped(_ sender: UIButton) {
         
@@ -91,16 +75,21 @@ class CommentVC: UIViewController {
         settingActionSheet.modalPresentationStyle = .overCurrentContext
         self.present(settingActionSheet, animated: true, completion: nil)
     }
+
     
 }
+
 //MARK: - Private Method
 extension CommentVC {
     
-    private func adjustTableViewHeight() {
-        commentTableViewHeight.constant = commentTableView.contentSize.height
+    private func setCommentTableView() {
+        commentTableView.delegate = self
+        commentTableView.dataSource = self
+        commentTableView.estimatedRowHeight = 30
+        commentTableView.rowHeight = UITableView.automaticDimension
     }
     
-    private func setView() {
+    private func setPopupView() {
         popupBackgroundView.setPopupBackgroundView(to: view)
     }
     
@@ -110,13 +99,6 @@ extension CommentVC {
     
     @objc func closePopup() {
         popupBackgroundView.animatePopupBackground(false)
-    }
-    
-    private func setTableView() {
-        commentTableView.estimatedRowHeight = 30
-        commentTableView.rowHeight = UITableView.automaticDimension
-        commentTableView.delegate = self
-        commentTableView.dataSource = self
     }
 }
 
@@ -144,6 +126,8 @@ extension CommentVC: UITableViewDelegate, UITableViewDataSource {
         if indexPath.section == 0 {
             guard let header = tableView.dequeueReusableCell(withIdentifier: QuestionAnswerTVC.identifier, for: indexPath) as? QuestionAnswerTVC else { return UITableViewCell() }
             
+            header.moreAnswerButton.isHidden = isMoreButtonHidden ? true : false
+            header.profileView.isHidden = isMyAnswer
             return header
         } else {
             if indexPath.row == 0 {
@@ -154,8 +138,9 @@ extension CommentVC: UITableViewDelegate, UITableViewDataSource {
                 
                 if commentArray[indexPath.section-1].children!.count == 0 {
                     // 댓글 한개일 경우 "답글 보기" 없애는 로직 ******************고치기***********************
-                    comment.moreCommentViewHeight.constant = 0
+                    comment.moreCommentView.isHidden = true
                 } else {
+                    comment.moreCommentView.isHidden = false
                     if commentArray[indexPath.section-1].open {
                         comment.moreCommentLabel.text = "답글 접기"
                         comment.moreImageView.image = UIImage(named: "icArrowUp")
@@ -177,11 +162,9 @@ extension CommentVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.section == 0 {
-            return 44.0
-        } else {
-            return UITableView.automaticDimension
-        }
+        
+        return UITableView.automaticDimension
+        
     }
     
     
@@ -205,17 +188,9 @@ extension CommentVC: UITableViewButtonSelectedDelegate {
                     }
                     
                     let section = IndexSet.init(integer: indexPath.section)
+                    commentTableView.reloadSections(section, with: .fade)
                     
-                    UIView.animate(withDuration: 0.2, animations: {
-                        self.commentTableView.reloadSections(section, with: .fade)
-                        
-                    }) { (_) in
-                        //self.commentTableView.layoutIfNeeded()
-                        self.adjustTableViewHeight()
-                        print(self.commentTableView.contentSize.height)
-                        
-                        
-                    }
+                    
                 }
             }
         }
