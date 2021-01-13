1ca684f9f0b57bef9795c26c6b8e45ccac3c09c0
jaeyong <maxwodyd98@gmail.com>
2021-01-14 04:58:46 +0900

    :hammer; 설정 화면 구현

---
+++ BeMe/BeMe/Setting/Cells/SettingTVC.swift
@@ -0,0 +1,23 @@
+//
+//  SettingTVC.swift
+//  BeMe
+//
+//  Created by 이재용 on 2021/01/14.
+//
+
+import UIKit
+
+class SettingTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
