8102fa2fea5bc054ea34fce215ce9a9add010fe4
jaeyong <maxwodyd98@gmail.com>
2021-01-15 21:53:12 +0900

    :report: 리드미 작성

---
+++ BeMe/BeMe/Extensions/CALayer+Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  CALayer+Extensions.swift
+//  BeMe
+//
+//  Created by 이재용 on 2021/01/15.
+//
+
+import UIKit
+
+
+extension CALayer {
+    func addBorder(_ arr_edge: [UIRectEdge], color: UIColor, width: CGFloat) {
+        for edge in arr_edge {
+            let border = CALayer()
+            switch edge {
+            case UIRectEdge.top:
+                border.frame = CGRect.init(x: 0, y: 0, width: frame.width, height: width)
+                break
+            case UIRectEdge.bottom:
+                border.frame = CGRect.init(x: 0, y: frame.height - width, width: frame.width, height: width)
+                break
+            case UIRectEdge.left:
+                border.frame = CGRect.init(x: 0, y: 0, width: width, height: frame.height)
+                break
+            case UIRectEdge.right:
+                border.frame = CGRect.init(x: frame.width - width, y: 0, width: width, height: frame.height)
+                break
+            default:
+                break
+            }
+            border.backgroundColor = color.cgColor;
+            self.addSublayer(border)
+        }
+    }
+}
